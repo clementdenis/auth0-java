ba70c08888e45bb459d09ab4eec45e98e3ca8186
clement <clement.denis@gmail.com>
2022-12-16 09:37:23 +0100

    Add filters to LogStream

--- src/main/java/com/auth0/json/mgmt/logstreams/LogStream.java
+++ src/main/java/com/auth0/json/mgmt/logstreams/LogStream.java
@@ -2,6 +2,7 @@ package com.auth0.json.mgmt.logstreams;
 
 import com.fasterxml.jackson.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,6 +22,8 @@ public class LogStream {
     private String status;
     @JsonProperty("sink")
     private Map<String, Object> sink;
+    @JsonProperty("filters")
+    private List<LogStreamFilter> filters;
 
     /**
      * Creates a new LogStream instance and sets the type, which cannot be changed once set.
@@ -118,5 +121,15 @@ public class LogStream {
     @JsonProperty("sink")
     public void setSink(Map<String, Object> sink) {
         this.sink = sink;
+    }
+
+    @JsonProperty("filters")
+    public List<LogStreamFilter> getFilters() {
+        return filters;
+    }
+
+    @JsonProperty("filters")
+    public void setFilters(List<LogStreamFilter> filters) {
+        this.filters = filters;
     }
 }
---
+++ src/main/java/com/auth0/json/mgmt/logstreams/LogStreamFilter.java
@@ -0,0 +1,42 @@
+package com.auth0.json.mgmt.logstreams;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LogStreamFilter {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("name")
+    private String name;
+
+    @JsonCreator
+    public LogStreamFilter(@JsonProperty("type") String type, @JsonProperty("name") String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+}
